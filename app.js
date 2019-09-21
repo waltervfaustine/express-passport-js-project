f9ab223a10a9f2756be8a645abd2d5808c8b3cc9
Walter V. Faustine <waltervfaustine@gmail.com>
2019-09-21 15:09:24 +0300

    feat: 🎸 added users and base routes to the routes folder

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// Base Routes
+app.use('/', require('./routes/index'));
+
+// User Routes
+app.use('/', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
